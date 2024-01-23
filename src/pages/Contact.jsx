c35ee720f841a1d63aa538996f6bf1191c7161cb
LampkinSynK <cejajaylin5@gmail.com>
2024-01-22 21:48:20 -0500

    contact validation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,67 @@
 import React from 'react'
 import '../stylesheets/contact.css'
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(() => {
+    const firstName = document.getElementById('fname');
+    const lastName = document.getElementById('lname');
+    const email = document.getElementById('email');
+    const phoneNumber = document.getElementById('p_num');
+    const message = document.getElementById('message');
+    var firstNameValid = null;
+    var lastNameValid = null;
+    var emailValid = null;
+    var phoneNumberValid = null;
+
+    firstName.addEventListener('keyup', function(){
+      var nameValue = firstName.value;
+      if (nameValue.match(/^[A-Za-z]*$/)) {
+          firstName.style.backgroundColor = 'rgb(185, 206, 154)';
+          firstNameValid = true;
+      }
+      else {
+          firstName.style.backgroundColor = 'red';
+          firstNameValid = false;
+      }
+  })
+  
+  lastName.addEventListener('keyup', function(){
+      var lastNameValue = lastName.value;
+      if (lastNameValue.match(/^[A-Za-z]*$/)) {
+          lastName.style.backgroundColor = 'rgb(185, 206, 154)';
+          lastNameValid = true;
+      }
+      else {
+          lastName.style.backgroundColor = 'red';
+          lastNameValid = false;
+      }
+  })
+  
+  email.addEventListener('keyup', function(){
+      var emailValue = email.value;
+      if (emailValue.match(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/)) {
+          email.style.backgroundColor = 'rgb(185, 206, 154)';
+          emailValid = true;
+      }
+      else {
+          email.style.backgroundColor = 'red';
+          emailValid = false;
+      }
+  })
+  
+  phoneNumber.addEventListener('keyup', function(){
+      var phoneValue = phoneNumber.value;
+      if (phoneValue.match(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im)) {
+          phoneNumber.style.backgroundColor = 'rgb(185, 206, 154)';
+          phoneNumberValid = true;
+      }
+      else {
+          phoneNumber.style.backgroundColor = 'red';
+          phoneNumberValid = false;
+      }
+  })
+  })
   return (
     <div class="chat_form">
         <form>
